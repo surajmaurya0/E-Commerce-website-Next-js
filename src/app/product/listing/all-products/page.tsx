9cfc257eac40fa3e1ec065accf0c68a064c412ef
surajmaurya0 <surajdevv1@gmail.com>
2023-12-20 17:04:55 +0530

    extra way

--- src/app/product/listing/all-products/page.tsx
+++
@@ -1,8 +0,0 @@
-import CommonListing from "@/components/CommonListing"
-import { getAllProduct } from "@/services/product"
-const AllProduct =async() =>{
-const allProduct = await getAllProduct()
-    return  <CommonListing data={allProduct.data}/>
-     
-}
-export default AllProduct
